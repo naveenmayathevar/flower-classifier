0fb87e2dbaddc4f7e95a6c3fc8d7233420ef77b5
Naveen Mayathevar <naveenmayathevar@gmail.com>
2025-05-25 03:45:57 -0700

    Initial commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Load model and class names
+
 model = load_model('mobilenetv2_best.h5')
 with open("class_names.json", "r") as f:
     class_names = json.load(f)
@@ -27,12 +27,12 @@ def index():
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             img_file.save(path)
 
-            # Preprocess image
+            
             img = image.load_img(path, target_size=(224, 224))
             img_array = image.img_to_array(img) / 255.0
             img_array = np.expand_dims(img_array, axis=0)
 
-            # Predict
+            
             preds = model.predict(img_array)
             class_idx = np.argmax(preds)
             confidence = round(100 * np.max(preds), 2)
@@ -41,4 +41,5 @@ def index():
     return render_template("index.html", prediction=prediction, filename=filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+
